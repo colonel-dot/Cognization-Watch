dbffd2fb8e7f6b376525f3503e9b008b6148f100
高远 <3037185932@qq.com>
2026-01-21 09:31:07 +0800

    准备设置选择舒尔特方格标准

--- cognitive/src/main/java/risk/vm/RiskViewModel.kt
+++ cognitive/src/main/java/risk/vm/RiskViewModel.kt
@@ -19,7 +19,7 @@ class RiskViewModel(application: Application) : AndroidViewModel(application) {
     fun queryRiskByDate(date: LocalDate) {
         viewModelScope.launch {
             try {
-                val risk = riskDao.getByDate(date)   // ⭐ 可能为 null，天然安全
+                val risk = riskDao.getByDate(date)   //  可能为 null，天然安全
                 _riskData.postValue(risk)
             } catch (e: Exception) {
                 _riskData.postValue(null)  // 任何异常也当作“无风险记录”
